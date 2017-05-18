b1c888db201205c30f283065ba4dea790e907811
Soumya <soumyasr@google.com>
2017-05-18 10:38:35 -0700

    Add a special test-only module for easier assertions in tests

---
+++ testing/module/module.go
@@ -0,0 +1,135 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package module provides a test module that can be used in tests.
+package module
+
+import (
+	"github.com/stretchrcom/testify/assert"
+
+	"github.com/soumya92/barista/bar"
+)
+
+// TestModule represents a bar.Module used for testing.
+type TestModule struct {
+	assert  *assert.Assertions
+	outChan chan bar.Output
+	sigChan chan interface{}
+	started bool
+	pauses  []bool
+	events  []bar.Event
+	outputs []bar.Output
+}
+
+// New creates a new module with the given testingT that can be used
+// to assert the behaviour of the bar (or related modules).
+func New(t assert.TestingT) *TestModule {
+	return &TestModule{
+		assert:  assert.New(t),
+		outChan: make(chan bar.Output),
+		sigChan: make(chan interface{}, 1),
+	}
+}
+
+// Stream conforms to bar.Module.
+func (t *TestModule) Stream() <-chan bar.Output {
+	if t.started {
+		panic("already streaming!")
+	}
+	go t.looper()
+	t.started = true
+	return t.outChan
+}
+
+// Click conforms to bar.Clickable.
+func (t *TestModule) Click(e bar.Event) {
+	t.events = append(t.events, e)
+}
+
+// Pause conforms to bar.Pausable.
+func (t *TestModule) Pause() {
+	t.pauses = append(t.pauses, true)
+}
+
+// Resume conforms to bar.Pausable.
+func (t *TestModule) Resume() {
+	t.pauses = append(t.pauses, false)
+}
+
+// Output queues output to be sent over the channel on the next read.
+func (t *TestModule) Output(out bar.Output) {
+	t.outputs = append(t.outputs, out)
+	select {
+	case t.sigChan <- nil:
+	default:
+	}
+}
+
+// AssertStarted asserts that the module was started.
+func (t *TestModule) AssertStarted(message string) {
+	t.assert.True(t.started, message)
+}
+
+// AssertPaused asserts that the module was paused,
+// and consumes the pause invocation.
+func (t *TestModule) AssertPaused(message string) {
+	t.assert.True(t.pauses[0], message)
+	// clear after assertion so sequential assertions match sequential events.
+	t.pauses = t.pauses[1:]
+}
+
+// AssertResumed asserts that the module was resumed,
+// and consumes the resume invocation.
+func (t *TestModule) AssertResumed(message string) {
+	t.assert.False(t.pauses[0], message)
+	t.pauses = t.pauses[1:]
+}
+
+// AssertNoPauseResume asserts that this module had no pause/resume interactions.
+func (t *TestModule) AssertNoPauseResume(message string) {
+	t.assert.Empty(t.pauses, message)
+}
+
+// AssertClicked asserts that the module was clicked with the given event.
+// Calling this multiple times asserts multiple click events.
+func (t *TestModule) AssertClicked(expected bar.Event, message string) {
+	t.assert.Equal(t.events[0], expected, message)
+	t.events = t.events[1:]
+}
+
+// AssertNotClicked asserts that the module received no events.
+func (t *TestModule) AssertNotClicked(message string) {
+	t.assert.Empty(t.events, message)
+}
+
+// Reset clears the history of pause/resume/click/stream invocations,
+// flushes any buffered events and resets the output channel.
+func (t *TestModule) Reset() {
+	close(t.outChan)
+	t.outChan = make(chan bar.Output)
+	close(t.sigChan)
+	t.sigChan = make(chan interface{})
+	t.started = false
+	t.pauses = nil
+	t.events = nil
+	t.outputs = nil
+}
+
+func (t *TestModule) looper() {
+	for range t.sigChan {
+		for _, out := range t.outputs {
+			t.outChan <- out
+		}
+	}
+}
---
+++ testing/module/module_test.go
@@ -0,0 +1,133 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package module
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchrcom/testify/assert"
+
+	"github.com/soumya92/barista/bar"
+)
+
+func finishedWithin(f func(), timeout time.Duration) bool {
+	doneChan := make(chan interface{})
+	go func() {
+		f()
+		doneChan <- nil
+	}()
+	select {
+	case <-doneChan:
+		return true
+	case <-time.After(timeout):
+		return false
+	}
+}
+
+func TestSimple(t *testing.T) {
+	m := New(t)
+	assert.False(t, m.started, "Initially not started")
+	initialOutput := bar.Output{bar.NewSegment("hello")}
+	assert.True(t,
+		finishedWithin(func() { m.Output(initialOutput) }, time.Second),
+		"Output does not block before start")
+	ch := m.Stream()
+	m.AssertStarted("Started when streaming starts")
+	assert.Panics(t, func() { m.Stream() }, "Panics when streamed again")
+
+	secondOutput := bar.Output{bar.NewSegment("world")}
+	m.Output(secondOutput)
+	var out bar.Output
+	assert.True(t,
+		finishedWithin(func() { out = <-ch }, time.Second),
+		"Read from channel does not block when output is buffered")
+	assert.Equal(t, initialOutput, out, "Outputs are returned sequentially")
+	out = <-ch
+	assert.Equal(t, secondOutput, out, "Outputs are not dropped")
+	assert.False(t,
+		finishedWithin(func() { <-ch }, 10*time.Millisecond),
+		"Read from channel blocks when no output")
+}
+
+func TestClick(t *testing.T) {
+	m := New(t)
+	evt1 := bar.Event{X: 2}
+	evt2 := bar.Event{Y: 2}
+	evt3 := bar.Event{X: 1, Y: 1}
+
+	m.AssertNotClicked("no events initially")
+	m.Click(evt1)
+	m.Click(evt2)
+	m.AssertClicked(evt1, "events are ordered")
+	m.AssertClicked(evt2, "events are buffered")
+	m.AssertNotClicked("events cleared after assertions")
+	m.Click(evt3)
+	m.AssertClicked(evt3, "events resumed after cleared")
+	m.AssertNotClicked("no extra events")
+}
+
+func TestPause(t *testing.T) {
+	m := New(t)
+	m.Pause()
+	m.AssertPaused("paused")
+	m.AssertNoPauseResume("invocation consumed on assertion")
+	m.Pause()
+	m.Pause()
+	m.AssertPaused("repeated pause")
+	m.AssertPaused("repeated pause")
+	m.AssertNoPauseResume("repeated invocations consumed")
+	m.Resume()
+	m.AssertResumed("resumed")
+	m.Resume()
+	m.Resume()
+	m.AssertResumed("repeated resume")
+	m.AssertResumed("repeated resume")
+	m.AssertNoPauseResume("repeated invocations consumed")
+	m.Resume()
+	m.Resume()
+	m.Pause()
+	m.Resume()
+	m.Resume()
+	m.Pause()
+	m.AssertResumed("ordering")
+	m.AssertResumed("ordering")
+	m.AssertPaused("ordering")
+	m.AssertResumed("ordering")
+	m.AssertResumed("ordering")
+	m.AssertPaused("ordering")
+	m.AssertNoPauseResume("consumed")
+}
+
+func TestReset(t *testing.T) {
+	m := New(t)
+	m.Pause()
+	m.Resume()
+	m.Pause()
+	m.Output(bar.Output{})
+	m.Output(bar.Output{bar.NewSegment("test")})
+	m.Click(bar.Event{})
+	m.Stream()
+	m.AssertStarted("some assertions before reset")
+	m.AssertPaused("some assertions before reset")
+	m.Reset()
+	m.AssertNotClicked("reset resets events")
+	m.AssertNoPauseResume("reset resets pause/resume")
+	var ch <-chan bar.Output
+	assert.NotPanics(t, func() { ch = m.Stream() }, "start after reset")
+	assert.False(t,
+		finishedWithin(func() { <-ch }, 10*time.Millisecond),
+		"No previous output sent over channel")
+}
