bf3d6460e3e431fac1ffa0172a23bf55cd382abb
Soumya <soumyasr@google.com>
2018-07-15 23:24:38 -0700

    [travis] Run tests with -v, cleanup intermediate profiles

--- travis.sh
+++ travis.sh
@@ -9,13 +9,14 @@ go list ./... \
 | grep -v barista/samples \
 | tac \
 | xargs -n1 -P4 -IPKG sh -c \
-'go test -coverprofile=profiles/$(echo "PKG" | sed "s|/|_|g").out -race -covermode=atomic "PKG"'
+'go test -v -coverprofile=profiles/$(echo "PKG" | sed "s|/|_|g").out -race -covermode=atomic "PKG"'
 
 # Debug log tests need the build tag, otherwise the nop versions will be used.
-go test -tags debuglog -coverprofile=profiles/logging_real.out -race -covermode=atomic ./logging
+go test -v -tags debuglog -coverprofile=profiles/logging_real.out -race -covermode=atomic ./logging
 
 # Merge all code coverage reports.
 cat profiles/*.out > coverage.txt
+rm -rf profiles/
 
 # Run tests only for samples.
 # This is just to make sure that all samples compile.
