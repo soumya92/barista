5d773a1f468644c3364d226fc890b046cad101fe
Soumya <soumyasr@google.com>
2018-07-18 09:12:13 -0700

    Add tests for Ionicons

--- pango/icons/ionicons/ionicons_test.go
+++ pango/icons/ionicons/ionicons_test.go
@@ -14,4 +14,70 @@
 
 package ionicons
 
-// Empty test file for more accurate code coverage.
+import (
+	"os"
+	"testing"
+
+	"github.com/soumya92/barista/pango"
+	"github.com/soumya92/barista/testing/githubfs"
+	pangoTesting "github.com/soumya92/barista/testing/pango"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInvalid(t *testing.T) {
+	fs = afero.NewMemMapFs()
+	assert.Error(t, Load("/src/no-such-directory"))
+
+	afero.WriteFile(fs, "/src/ion-error-1/scripts/manifest.json", []byte(
+		`-- Invalid JSON --`,
+	), 0644)
+	assert.Error(t, LoadIos("/src/ion-error-1"))
+
+	afero.WriteFile(fs, "/src/ion-error-2/scripts/manifest.json", nil, 0644)
+	assert.Error(t, LoadMd("/src/ion-error-2"))
+
+	afero.WriteFile(fs, "/src/ion-error-3/scripts/manifest.json", []byte(
+		`{"icons": [
+			{"name": "someIcon", "code": "0x61"},
+			{"name": "otherIcon", "code": "0x62"},
+			{"name": "someIcon", "code": "0xghij"}
+		]}`,
+	), 0644)
+	assert.Error(t, Load("/src/ion-error-3"))
+}
+
+func TestValid(t *testing.T) {
+	fs = afero.NewMemMapFs()
+	afero.WriteFile(fs, "/src/ion/scripts/manifest.json", []byte(
+		`{"icons": [
+			{"name": "md-someIcon", "code": "0x61"},
+			{"name": "ios-someIcon", "code": "0x62"},
+			{"name": "otherIcon", "code": "0x63"}
+		]}`,
+	), 0644)
+	assert.NoError(t, Load("/src/ion"))
+	pangoTesting.AssertText(t, "a", pango.Icon("ion-md-someIcon").String())
+	pangoTesting.AssertText(t, "b", pango.Icon("ion-ios-someIcon").String())
+	pangoTesting.AssertText(t, "c", pango.Icon("ion-otherIcon").String())
+
+	assert.NoError(t, LoadMd("/src/ion"))
+	pangoTesting.AssertText(t, "a", pango.Icon("ion-someIcon").String())
+	pangoTesting.AssertText(t, "b", pango.Icon("ion-ios-someIcon").String())
+
+	assert.NoError(t, LoadIos("/src/ion"))
+	pangoTesting.AssertText(t, "b", pango.Icon("ion-someIcon").String())
+	pangoTesting.AssertText(t, "a", pango.Icon("ion-md-someIcon").String())
+}
+
+// TestLive tests that current master branch of the icon font works with
+// this package. This test only runs when CI runs tests in 'cron' mode,
+// which provides timely notifications of incompatible changes while
+// keeping default tests hermetic.
+func TestLive(t *testing.T) {
+	if evt := os.Getenv("TRAVIS_EVENT_TYPE"); evt != "cron" {
+		t.Skipf("Skipping LiveVersion test for event type '%s'", evt)
+	}
+	fs = githubfs.New()
+	assert.NoError(t, Load("/ionic-team/ionicons/master"))
+}
