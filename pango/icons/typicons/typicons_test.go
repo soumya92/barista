4ab2a799942614667960a98da8eedee1da9f22b7
Soumya <soumyasr@google.com>
2018-07-20 21:58:33 -0700

    Add tests for Typicons

--- pango/icons/typicons/typicons_test.go
+++ pango/icons/typicons/typicons_test.go
@@ -14,4 +14,68 @@
 
 package typicons
 
-// Empty test file for more accurate code coverage.
+import (
+	"os"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/soumya92/barista/pango"
+	"github.com/soumya92/barista/testing/githubfs"
+	pangoTesting "github.com/soumya92/barista/testing/pango"
+)
+
+func TestInvalid(t *testing.T) {
+	fs = afero.NewMemMapFs()
+	assert.Error(t, Load("/src/no-such-directory"))
+
+	afero.WriteFile(fs, "/src/typicons-error-1/config.yml", []byte(
+		`-- Invalid YAML --`,
+	), 0644)
+	assert.Error(t, Load("/src/typicons-error-1"))
+
+	afero.WriteFile(fs, "/src/typicons-error-2/config.yml", nil, 0644)
+	assert.Error(t, Load("/src/typicons-error-2"))
+
+	afero.WriteFile(fs, "/src/typicons-error-3/config.yml", []byte(
+		`glyphs:
+- css: someIcon
+  code: 0x61
+- css: otherIcon
+  code: 0x62
+- css: thirdIcon
+  code: 0xghij
+`,
+	), 0644)
+	assert.Error(t, Load("/src/typicons-error-3"))
+}
+
+func TestValid(t *testing.T) {
+	fs = afero.NewMemMapFs()
+	afero.WriteFile(fs, "/src/typicons/config.yml", []byte(
+		`glyphs:
+- css: someIcon
+  code: 0x61
+- css: otherIcon
+  code: 0x62
+- css: thirdIcon
+  code: 0x63
+`,
+	), 0644)
+	assert.NoError(t, Load("/src/typicons"))
+	pangoTesting.AssertText(t, "a", pango.Icon("typecn-someIcon").String())
+	pangoTesting.AssertText(t, "b", pango.Icon("typecn-otherIcon").String())
+}
+
+// TestLive tests that current master branch of the icon font works with
+// this package. This test only runs when CI runs tests in 'cron' mode,
+// which provides timely notifications of incompatible changes while
+// keeping default tests hermetic.
+func TestLive(t *testing.T) {
+	if evt := os.Getenv("TRAVIS_EVENT_TYPE"); evt != "cron" {
+		t.Skipf("Skipping LiveVersion test for event type '%s'", evt)
+	}
+	fs = githubfs.New()
+	assert.NoError(t, Load("/stephenhutchings/typicons.font/master"))
+}
