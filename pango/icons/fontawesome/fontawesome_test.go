7fedbf1073525d9c7347f52990f5b06b863d9421
Soumya <soumyasr@google.com>
2018-07-20 22:36:38 -0700

    Add tests for fontawesome

--- pango/icons/fontawesome/fontawesome_test.go
+++ pango/icons/fontawesome/fontawesome_test.go
@@ -14,4 +14,53 @@
 
 package fontawesome
 
-// Empty test file for more accurate code coverage.
+import (
+	"os"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/soumya92/barista/pango"
+	"github.com/soumya92/barista/testing/githubfs"
+	pangoTesting "github.com/soumya92/barista/testing/pango"
+)
+
+func TestInvalid(t *testing.T) {
+	fs = afero.NewMemMapFs()
+	assert.Error(t, Load("/src/no-such-directory"))
+
+	afero.WriteFile(fs, "/src/fa-error-1/web-fonts-with-css/scss/_variables.scss", []byte(
+		`
+$blah: "red";
+$fa-var-xy: xy;
+$fa-var-foobar: \61;
+`,
+	), 0644)
+	assert.Error(t, Load("/src/fa-error-1"))
+}
+
+func TestValid(t *testing.T) {
+	fs = afero.NewMemMapFs()
+	afero.WriteFile(fs, "/src/fa/web-fonts-with-css/scss/_variables.scss", []byte(
+		`
+$fa-var-some-icon: \61;
+$fa-var-other-icon: \62;
+`,
+	), 0644)
+	assert.NoError(t, Load("/src/fa"))
+	pangoTesting.AssertText(t, "a", pango.Icon("fa-some-icon").String())
+	pangoTesting.AssertText(t, "b", pango.Icon("fa-other-icon").String())
+}
+
+// TestLive tests that current master branch of the icon font works with
+// this package. This test only runs when CI runs tests in 'cron' mode,
+// which provides timely notifications of incompatible changes while
+// keeping default tests hermetic.
+func TestLive(t *testing.T) {
+	if evt := os.Getenv("TRAVIS_EVENT_TYPE"); evt != "cron" {
+		t.Skipf("Skipping LiveVersion test for event type '%s'", evt)
+	}
+	fs = githubfs.New()
+	assert.NoError(t, Load("/FortAwesome/Font-Awesome/master"))
+}
