c66c43a80f523ed5dc3f56876cca557a85023a84
Soumya <soumyasr@google.com>
2018-08-05 18:51:33 -0700

    Add CodeClimate test coverage

--- travis.sh
+++ travis.sh
@@ -4,6 +4,13 @@
 set -e
 mkdir -p profiles/
 
+CODECLIMATE=0
+if curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 >./cc-test-reporter; then
+	CODECLIMATE=1
+	chmod +x ./cc-test-reporter
+	./cc-test-reporter before-build
+fi
+
 # Run tests with coverage for all barista packages
 go list ./... \
 | grep -v barista/samples \
@@ -21,3 +28,10 @@ rm -rf profiles/
 # Run tests only for samples.
 # This is just to make sure that all samples compile.
 go test ./samples/...
+
+# Codecov.io wants coverage.txt, but CodeClimate wants c.out.
+if [ $CODECLIMATE -eq 1 ]; then
+	echo "mode: count" > c.out
+	grep -h -v "^mode:" coverage.txt >> c.out
+	./cc-test-reporter after-build
+fi
