6f1322fbe079dbd42f6e13fe8be979af21e3e267
Soumya <soumyasr@google.com>
2018-09-20 09:26:52 -0700

    Better build script for automated builds

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+# Save keys to ~/.config/barista/keys to avoid leaking them to git.
+# Or use your preferred way to set them as environment variables.
+CONFIG_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}"
+[ -e "$CONFIG_DIR/barista/keys" ] && . "$CONFIG_DIR/barista/keys"
+
+# Any KEYS will be inserted into the sample bar by replacing
+# '%%$NAME_OF_KEY%%' with the value of $NAME_OF_KEY from the environment.
+# e.g. '%%OWM_API_KEY%%' in the go file will be replaced with the value
+# of $OWM_API_KEY.
+KEYS=(
+	'GITHUB_CLIENT_ID' 'GITHUB_CLIENT_SECRET'
+	'GOOGLE_CLIENT_ID' 'GOOGLE_CLIENT_SECRET'
+	'OWM_API_KEY'
+)
+
+TARGET_FILE="./samples/sample-bar/sample-bar.go"
+
+# Save the current sample-bar, so we can revert it after building, to
+# prevent accidentally checking in the keys. We can't use git checkout
+# because the file could have other modifications that aren't committed.
+BACKUP_FILE="$(mktemp)"
+cp "$TARGET_FILE" "$BACKUP_FILE"
+function restore {
+	cp "$BACKUP_FILE" "$TARGET_FILE"
+	rm -f "$BACKUP_FILE"
+}
+trap restore EXIT
+
+for KEY in ${KEYS[@]}; do
+	if [ -n "${!KEY}" ]; then
+		sed -i "s/%%${KEY}%%/${!KEY}/g" "$TARGET_FILE"
+	else
+		echo "Skipping $KEY, value not set" >&2
+	fi
+done
+
+# Build the sample bar with all the keys set. Pass all arguments to the
+# `go build` command, allowing e.g. `./build.sh -o ~/bin/mybar`, or even
+# `./build.sh -race -tags debuglog`.
+go build "$@" -i ./samples/sample-bar
+
