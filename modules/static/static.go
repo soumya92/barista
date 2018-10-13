739d409b3ed3fbff41606b212c55d46841d14e5f
Soumya <soumyasr@google.com>
2018-10-13 10:11:06 -0700

    Add modules/static: a settable module for static output

---
+++ modules/static/static.go
@@ -0,0 +1,56 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package static provides a simple module that shows static content on the bar,
+// with methods to set the content. In a pinch, this can be used to create
+// buttons, or show additional information by setting the output from within
+// a format function.
+package static // import "barista.run/modules/static"
+
+import (
+	"barista.run/bar"
+	"barista.run/base/value"
+)
+
+// Module represents a module that displays static content on the bar.
+type Module struct {
+	output value.Value
+}
+
+// Stream starts the module.
+func (m *Module) Stream(sink bar.Sink) {
+	for {
+		next := m.output.Next()
+		out, _ := m.output.Get().(bar.Output)
+		sink(out)
+		<-next
+	}
+}
+
+// Set sets the output to display.
+func (m *Module) Set(out bar.Output) {
+	m.output.Set(out)
+}
+
+// Clear sets an empty output.
+func (m *Module) Clear() {
+	m.output.Set(nil)
+}
+
+// New constructs a static module that displays the given output.
+func New(initial bar.Output) *Module {
+	m := new(Module)
+	m.Set(initial)
+	return m
+}
---
+++ modules/static/static_test.go
@@ -0,0 +1,75 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package static
+
+import (
+	"testing"
+	"time"
+
+	"barista.run/bar"
+	"barista.run/outputs"
+	testBar "barista.run/testing/bar"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStatic(t *testing.T) {
+	testBar.New(t)
+
+	s0 := New(nil)
+	s1 := New(outputs.Text("foo"))
+	s2 := new(Module)
+	testBar.Run(s0, s1, s2)
+
+	testBar.LatestOutput().AssertText([]string{"foo"}, "on start")
+	testBar.AssertNoOutput("no change to static modules")
+
+	s0.Set(outputs.Text("baz"))
+	testBar.LatestOutput(0).AssertText([]string{"baz", "foo"})
+
+	s1.Clear()
+	testBar.LatestOutput(1).AssertText([]string{"baz"})
+
+	s2.Clear()
+	testBar.LatestOutput(2).AssertText([]string{"baz"})
+
+	clickCh := make(chan bool, 1)
+	s2.Set(outputs.Text("foo").OnClick(func(bar.Event) { clickCh <- true }))
+
+	out := testBar.LatestOutput(2)
+	out.AssertText([]string{"baz", "foo"})
+
+	select {
+	case <-clickCh:
+		require.Fail(t, "spurious click event")
+	case <-time.After(10 * time.Millisecond):
+		// test passed
+	}
+
+	out.At(0).LeftClick()
+	select {
+	case <-clickCh:
+		require.Fail(t, "spurious click event")
+	case <-time.After(10 * time.Millisecond):
+		// test passed
+	}
+
+	out.At(1).LeftClick()
+	select {
+	case v := <-clickCh:
+		require.True(t, v, "click handler triggered")
+	case <-time.After(time.Second):
+		require.Fail(t, "click event not received")
+	}
+}
