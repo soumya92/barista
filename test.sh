115db23271d9ded234a213b1ffc4ce2c2bc41635
Soumya <soumyasr@google.com>
2018-10-19 21:20:15 -0700

    test.sh improvements
    - Autoselect parallel count based on available CPUs
    - Better support for running from symlinked directories (i.e. source outside GOPATH, symlinked to GOPATH/src/barista.run)
    - Print some logs indicating the lint/vet/test stages
    - Produce valid coverage.txt that covers the entire source tree (works with go tool cover)

--- test.sh
+++ test.sh
@@ -12,35 +12,49 @@ if test -n "$CC_TEST_REPORTER_ID" && curl -LSs "$CC_TEST_REPORTER_LOC" >./cc-tes
 	./cc-test-reporter before-build
 fi
 
+# Since quite a few tests have sleeps, running nproc + 2 tests should result in most
+# effective parallelisation.
+NPAR="$(($(nproc) + 2))"
+
 # For local runs, use golint from PATH,
 GOLINT="$(which golint)"
 # but fallback to the CI path otherwise.
 [ -n "$GOLINT" ] || GOLINT="$HOME/gopath/bin/golint"
 
+echo "Lint: $GOLINT ./..."
 $GOLINT ./...
+
+echo "Vet: go vet"
 go vet
 
+echo "Test: Running $NPAR in parallel"
 # Run tests with coverage for all barista packages
 go list ./... \
 | grep -v /samples/ \
+| sed "s|_$PWD|.|" \
 | tac \
-| xargs -n1 -P4 -IPKG sh -c \
-'go test -coverprofile=profiles/$(echo "PKG" | sed "s|/|_|g").out -race -covermode=atomic "PKG"'
+| xargs -n1 -P$NPAR -IPKG sh -c \
+'go test -coverprofile=profiles/$(echo "PKG" | sed -e "s|./||" -e "s|/|_|g").out -race -covermode=atomic "PKG"'
 
+echo "Test: Logging with -tags debuglog"
 # Debug log tests need the build tag, otherwise the nop versions will be used.
-go test -tags debuglog -coverprofile=profiles/logging_real.out -race -covermode=atomic ./logging
+go test -tags debuglog -coverprofile=profiles/logging_real.out -race -covermode=atomic barista.run/logging
 
-# Merge all code coverage reports.
-cat profiles/*.out > coverage.txt
+# Merge all code coverage reports. Although codecov does something similar internally,
+# doing it here means that after running ./test.sh, you can run
+#     go tool cover -html=coverage.txt
+# and it will show a coverage report instead of complaining about a bad format.
+grep -E '^mode: \w+$' "$(find profiles/ -name '*.out' -print -quit)" > coverage.txt
+grep -hEv '^(mode: \w+)?$' profiles/*.out >> coverage.txt
 rm -rf profiles/
 
+echo "Test: Samples"
 # Run tests only for samples.
 # This is just to make sure that all samples compile.
 go test ./samples/...
 
 # Codecov.io wants coverage.txt, but CodeClimate wants c.out.
 if [ $CODECLIMATE -eq 1 ]; then
-	echo "mode: count" > c.out
-	grep -h -v "^mode:" coverage.txt >> c.out
+	cp "coverage.txt" "c.out"
 	./cc-test-reporter after-build
 fi
