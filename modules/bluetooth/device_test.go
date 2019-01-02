a377b2bfdea24e79fba042ad569902da31881ac5
Dale Campbell <dale.campbell@audiogon.com>
2019-01-02 16:14:25 -0500

    Adding device module test.  Issue #55

---
+++ modules/bluetooth/device_test.go
@@ -0,0 +1,62 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bluetooth
+
+import (
+	"strings"
+	"testing"
+
+	godbus "github.com/godbus/dbus"
+
+	"barista.run/bar"
+	"barista.run/base/watchers/dbus"
+	"barista.run/outputs"
+	testBar "barista.run/testing/bar"
+)
+
+func TestDevice(t *testing.T) {
+	testBar.New(t)
+	bus := dbus.SetupTestBus()
+	bluez := bus.RegisterService("org.bluez")
+
+	adapterName := "hci0"
+	deviceMac := "00:00:00:00:25:9F"
+	devicePath := "dev_" + strings.Replace(deviceMac, ":", "_", -1)
+	deviceObjPath := godbus.ObjectPath("/org/bluez/" + adapterName + "/" + devicePath)
+	device := bluez.Object(deviceObjPath, "org.bluez.Device1")
+	device.SetProperties(map[string]interface{}{
+		"Name":      "foo",
+		"Alias":     "foo alias",
+		"Address":   deviceMac,
+		"Paired":    true,
+		"Connected": true,
+		"Trusted":   true,
+		"Blocked":   false,
+	}, dbus.SignalTypeNone)
+
+	devModule := Device(adapterName, deviceMac)
+	devModule.Output(func(i DeviceInfo) bar.Output {
+		paired := "NO"
+		if i.Paired {
+			paired = "YES"
+		}
+		return outputs.Textf("%s: %s", i.Name, paired)
+	})
+	testBar.Run(devModule)
+
+	testBar.LatestOutput().AssertText([]string{
+		"foo: YES",
+	})
+}
