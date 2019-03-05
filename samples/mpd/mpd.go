7047742f536f7b0d09aa88c84b79f009c735a70b
Christian Rebischke <chris@nullday.de>
2019-03-06 00:54:04 +0100

    still playing around with the structure
    Idea: change the structure to a similar one like the MPRIS info
    structure and I really have to look into the purpose of that Stream
    function. I still have trouble to use it correctly, I guess.
    
    Signed-off-by: Christian Rebischke <chris@nullday.de>

--- samples/mpd/mpd.go
+++ samples/mpd/mpd.go
@@ -1,16 +1,20 @@
 package mpd
 
 import (
+	"barista.run/bar"
+	"barista.run/base/value"
+	"barista.run/outputs"
 	"github.com/fhs/gompd/mpd"
 	"strconv"
 )
+
+// Global MPD connection
+var conn *mpd.Client
 
 // MPD represents the current state and the current song
 type MPD struct {
 	status      mpd.Attrs
 	currentSong mpd.Attrs
-	host        string
-	port        int64
 }
 
 // Status returns the current state of MPD as map[string]string aka mpd.Attrs
@@ -23,28 +27,45 @@ func (m *MPD) CurrentSong() mpd.Attrs {
 	return m.currentSong
 }
 
-// setHost sets the host for the MPD Dial
-func (m *MPD) setHost(host string) {
-	m.host = host
-}
-
-// setPort sets the port for the MPD Dial
-func (m *MPD) setPort(port int64) {
-	m.port = port
-}
-
-func (m *MPD) setStatus() {
-	parsedPort := strconv.FormatInt(m.port, 10)
-	conn, _ := mpd.Dial("tcp", m.host+":"+parsedPort)
+func (m *MPD) setupConnection(host string, port uint64)  {
+	parsedPort := strconv.FormatUint(port, 10)
+	var err error
+	conn, err = mpd.Dial("tcp", host+":"+parsedPort)
+	if err != nil {
+		conn = nil
+	}
 	status, _ := conn.Status()
 	m.status = status
-}
-
-func (m *MPD) setCurrentSong() {
-	parsedPort := strconv.FormatInt(m.port, 10)
-	conn, _ := mpd.Dial("tcp", m.host+":"+parsedPort)
 	if m.status["state"] == "play" {
 		currentSong, _ := conn.CurrentSong()
 		m.currentSong = currentSong
 	}
+}
+
+type Module struct {
+	host        string
+	port        uint64
+	outputFunc value.Value
+}
+
+// Output configures a module to display the output of a user-defined function.
+func (m *Module) Output(outputFunc func(MPD) bar.Output) *Module {
+	m.outputFunc.Set(outputFunc)
+	return m
+}
+
+func (m *Module) Stream(s bar.Sink) {
+	var c MPD
+	c.setupConnection(m.host, m.port)
+}
+
+func New(host string, port uint64) *Module {
+	m := &Module {
+		host: host,
+		port: port,
+	}
+	m.Output(func(c MPD) bar.Output {
+		return outputs.Textf("status: %s current song: %s -- %s", c.status["state"], c.currentSong["Artist"], c.currentSong["Title"])
+	})
+	return m
 }
