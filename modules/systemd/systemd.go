1feca842dbaba5ac9a689916c03d36a168871b4d
Karolos Lykos <l.g.karolos@gmail.com>
2023-04-06 22:46:22 +0300

    Update systemd module to include user units (#374)
    Issue: #294 
    
    Added an optional busType parameter in the Service function on the
    systemd module in order to not introduce a breaking change.
    
    I tested locally and seems to work just fine

--- modules/systemd/systemd.go
+++ modules/systemd/systemd.go
@@ -65,7 +65,7 @@ type UnitInfo struct {
 	call func(string, ...interface{}) ([]interface{}, error)
 }
 
-// Start enqeues a start job, and possibly depending jobs.
+// Start enqueues a start job, and possibly dependant jobs.
 func (u UnitInfo) Start() {
 	u.call("Start", "fail")
 }
@@ -87,13 +87,10 @@ func (u UnitInfo) Reload() {
 	u.call("Reload", "fail")
 }
 
-// replaced in tests.
-var busType = dbus.System
-
-func watchUnit(unitName string) *dbus.PropertiesWatcher {
-	escapedName := systemdbus.PathBusEscape(unitName)
+func watchUnit(name string, busType ...dbus.BusType) *dbus.PropertiesWatcher {
+	escapedName := systemdbus.PathBusEscape(name)
 	unitPath := "/org/freedesktop/systemd1/unit/" + escapedName
-	return dbus.WatchProperties(busType,
+	return dbus.WatchProperties(getBusType(busType...),
 		"org.freedesktop.systemd1", unitPath, "org.freedesktop.systemd1.Unit").
 		Add("ActiveState", "SubState", "Id", "Description").
 		FetchOnSignal("StateChangeTimestamp")
@@ -110,12 +107,22 @@ type ServiceInfo struct {
 // ServiceModule watches a systemd service and updates on status change
 type ServiceModule struct {
 	name       string
+	busType    dbus.BusType
 	outputFunc value.Value
+}
+
+// UserService create a module that watches the status of a systemd user service.
+func UserService(name string) *ServiceModule {
+	return service(name, dbus.Session)
 }
 
 // Service creates a module that watches the status of a systemd service.
 func Service(name string) *ServiceModule {
-	s := &ServiceModule{name: name}
+	return service(name, dbus.System)
+}
+
+func service(name string, dbusType dbus.BusType) *ServiceModule {
+	s := &ServiceModule{name: name, busType: dbusType}
 	s.Output(func(i ServiceInfo) bar.Output {
 		if i.Since.IsZero() {
 			return outputs.Textf("%s (%s)", i.State, i.SubState)
@@ -139,7 +146,7 @@ const serviceIface = "org.freedesktop.systemd1.Service"
 
 // Stream starts the module.
 func (s *ServiceModule) Stream(sink bar.Sink) {
-	w := watchUnit(s.name + ".service")
+	w := watchUnit(s.name+".service", s.busType)
 	defer w.Unsubscribe()
 
 	w.FetchOnSignal(
@@ -204,6 +211,14 @@ func getServiceInfo(w *dbus.PropertiesWatcher) ServiceInfo {
 	return i
 }
 
+func getBusType(busType ...dbus.BusType) dbus.BusType {
+	if len(busType) == 0 {
+		return dbus.System
+	}
+
+	return busType[0]
+}
+
 // TimerInfo represents the state of a systemd timer.
 type TimerInfo struct {
 	UnitInfo
@@ -215,12 +230,22 @@ type TimerInfo struct {
 // TimerModule watches a systemd timer and updates on status change
 type TimerModule struct {
 	name       string
+	busType    dbus.BusType
 	outputFunc value.Value
+}
+
+// UserTimer creates a module that watches the status of a systemd user timer.
+func UserTimer(name string) *TimerModule {
+	return timer(name, dbus.Session)
 }
 
 // Timer creates a module that watches the status of a systemd timer.
 func Timer(name string) *TimerModule {
-	t := &TimerModule{name: name}
+	return timer(name, dbus.System)
+}
+
+func timer(name string, dbusType dbus.BusType) *TimerModule {
+	t := &TimerModule{name: name, busType: dbusType}
 	t.Output(func(i TimerInfo) bar.Output {
 		last := "never"
 		if !i.LastTrigger.IsZero() {
@@ -245,7 +270,7 @@ const timerIface = "org.freedesktop.systemd1.Timer"
 
 // Stream starts the module.
 func (t *TimerModule) Stream(sink bar.Sink) {
-	w := watchUnit(t.name + ".timer")
+	w := watchUnit(t.name+".timer", t.busType)
 	defer w.Unsubscribe()
 
 	w.FetchOnSignal(
